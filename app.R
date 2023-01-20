28a044de421783c8efcf3f87b99d88b966902a08
maxsettineri <maxsettineri@bren.ucsb.edu>
2023-01-19 16:08:27 -0800

    r bein weird

---
+++ app.R
@@ -0,0 +1,20 @@
+library(shiny)
+library(tidyverse)
+library(palmerpenguins)
+
+### Create a user interface
+ui<- fluidPage(
+  titlePanel('I am ading a title'),
+  sidebarLayout(
+    sidebarPanel('Put my widgets here'),
+    mainPanel('Put my graph here')
+  ) ### end sidebarLayout
+) ### end fluidPage
+
+
+
+### Create the server function
+server <- function(input, output) {}
+
+### combine these into an app
+shinyApp(ui = ui, server = server)
